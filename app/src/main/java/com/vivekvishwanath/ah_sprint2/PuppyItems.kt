dff03359fe83d7eec7471abee9da163d4bc40040
VivekV95 <vivekv210@gmail.com>
2019-08-15 13:53:58 -0500

    Add puppy names and images

---
+++ app/src/main/java/com/vivekvishwanath/ah_sprint2/PuppyItems.kt
@@ -0,0 +1,45 @@
+package com.vivekvishwanath.ah_sprint2
+
+val puppyIds = arrayOf(
+    R.drawable.american_hairless_terrier,
+    R.drawable.bernese_mountain_dog,
+    R.drawable.black_lab,
+    R.drawable.border_collie,
+    R.drawable.chocolate_lab,
+    R.drawable.french_bulldog,
+    R.drawable.golden_retriever,
+    R.drawable.havanese,
+    R.drawable.irish_setter,
+    R.drawable.leonberger,
+    R.drawable.old_english_sheepdog,
+    R.drawable.poodle,
+    R.drawable.pug,
+    R.drawable.rottie,
+    R.drawable.shiba_inu,
+    R.drawable.shih_tzu,
+    R.drawable.siberian_husky,
+    R.drawable.welsh_springer,
+    R.drawable.wirehaired_pointing_griffon
+)
+
+val puppyNames = arrayOf(
+    "American Hairless Terrier",
+    "Bernese Mountain Dog",
+    "Black Lab",
+    "Border Collie",
+    "Chocolate Lab",
+    "French Bulldog",
+    "Golden Retriever",
+    "Havenese",
+    "Irish Setter",
+    "Leonberger",
+    "Old English Sheepdog",
+    "Poodle",
+    "Pug",
+    "Rottie",
+    "Shiba Inu",
+    "Shih Tzu",
+    "Siberian Husky",
+    "Welsh Springer",
+    "Wirehaired Pointing Griffon"
+)
