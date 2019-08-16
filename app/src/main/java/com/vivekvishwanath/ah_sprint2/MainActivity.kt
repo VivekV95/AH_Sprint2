e6d7682b8ae7329267888ed2b44578bb89defc6d
VivekV95 <vivekv210@gmail.com>
2019-08-15 20:20:11 -0500

    Create Puppy model

--- app/src/main/java/com/vivekvishwanath/ah_sprint2/MainActivity.kt
+++ app/src/main/java/com/vivekvishwanath/ah_sprint2/MainActivity.kt
@@ -3,6 +3,9 @@ package com.vivekvishwanath.ah_sprint2
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
+// Step 1: Make the Puppy model
+// Step 2: Process raw puppy data
+
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
---
+++ app/src/main/java/com/vivekvishwanath/ah_sprint2/Puppy.kt
@@ -0,0 +1,7 @@
+package com.vivekvishwanath.ah_sprint2
+
+data class Puppy(
+    val breed: String,
+    val imageId: Int,
+    var isFavorite: Boolean
+)
