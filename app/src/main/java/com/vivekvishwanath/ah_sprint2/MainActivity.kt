7487ee24cb0d975248494a0b9d67d7139863fbd8
VivekV95 <vivekv210@gmail.com>
2019-08-15 21:51:12 -0500

    Finish RecyclerView

--- app/src/main/java/com/vivekvishwanath/ah_sprint2/MainActivity.kt
+++ app/src/main/java/com/vivekvishwanath/ah_sprint2/MainActivity.kt
@@ -2,11 +2,14 @@ package com.vivekvishwanath.ah_sprint2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import kotlinx.android.synthetic.main.activity_main.*
 
 // Step 1: Make the Puppy model
 // Step 2: Process raw puppy data
 // Step 3: Design RV item layout
-// Step 4: Created adapter for RecyclerView 
+// Step 4: Created adapter for RecyclerView
+// Step 5: Finish RecyclerView
 
 class MainActivity : AppCompatActivity() {
 
@@ -14,5 +17,26 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        PuppyRepository.createPuppyList()
+
+        puppy_list_view.apply {
+            setHasFixedSize(true)
+            layoutManager = LinearLayoutManager(this@MainActivity)
+            adapter = PuppyListAdapter(PuppyRepository.puppyList)
+        }
+
+        favorites_button.setOnClickListener {
+            val favorites = getFavorites()
+            val i = 0
+        }
+    }
+
+    fun getFavorites(): String {
+        var favoritesString = ""
+        for (puppy in PuppyRepository.puppyList) {
+            if (puppy.isFavorite) favoritesString += "${puppy.breed}, "
+        }
+
+        return favoritesString
     }
 }
--- app/src/main/java/com/vivekvishwanath/ah_sprint2/PuppyListAdapter.kt
+++ app/src/main/java/com/vivekvishwanath/ah_sprint2/PuppyListAdapter.kt
@@ -4,11 +4,13 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
+import android.widget.LinearLayout
+import android.widget.TextView
 import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.RecyclerView
 import kotlinx.android.synthetic.main.puppy_list_item.view.*
 
-class PuppyListAdapter(val puppyList: MutableList<Puppy>): RecyclerView.Adapter<PuppyListAdapter.ViewHolder>() {
+class PuppyListAdapter(val puppyList: MutableList<Puppy>) : RecyclerView.Adapter<PuppyListAdapter.ViewHolder>() {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         return ViewHolder(LayoutInflater.from(parent.context)
                 .inflate(R.layout.puppy_list_item, parent, false) as View)
@@ -19,13 +21,28 @@ class PuppyListAdapter(val puppyList: MutableList<Puppy>): RecyclerView.Adapter<
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        holder.bindModel(puppyList[position])
+        val puppy = puppyList[position]
+        holder.bindModel(puppy)
+
+        holder.puppyItemParent.setOnClickListener {
+            if (puppy.isFavorite) {
+                puppy.isFavorite = false
+                notifyItemChanged(position)
+            } else {
+                puppy.isFavorite = true
+                notifyItemChanged(position)
+            }
+
+            notifyItemChanged(position)
+        }
+
+
     }
 
-    class ViewHolder(view: View): RecyclerView.ViewHolder(view) {
-        val puppyImageView = view.puppy_image_view
-        val puppyNameView = view.puppy_name_view
-        val puppyItemParent = view.puppy_item_parent
+    class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        val puppyImageView: ImageView = view.puppy_image_view
+        val puppyNameView: TextView = view.puppy_name_view
+        val puppyItemParent: LinearLayout = view.puppy_item_parent
 
         fun bindModel(puppy: Puppy) {
             puppyImageView.setImageResource(puppy.imageId)
